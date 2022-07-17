bd871a987fcdc51c15c56777f67b26dc4671a5e8
Sam <saint00771155@gmail.com>
2022-07-17 21:04:42 +0800

    Initialize commit

---
+++ RotateButton/RotateButton/RotateButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  RotateButtonApp.swift
+//  RotateButton
+//
+//  Created by Sam on 2022/7/17.
+//
+
+import SwiftUI
+
+@main
+struct RotateButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
